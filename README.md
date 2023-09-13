2ae254db15784e3137ffb418f8762f047207520b
Bantas93 <bantaz.mtdn@gmail.com>
2023-09-13 12:32:56 +0700

    push

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# portofolio-html
+# Portofolio Sederhana dengan HTML, CSS, JavaScript, dan API Gratis
+
+Ini adalah proyek portofolio sederhana yang dibangun dengan HTML, CSS, dan JavaScript. Proyek ini juga menggunakan API gratis untuk menampilkan data tentang proyek-proyek yang telah saya kerjakan.
+
+## Cara Menjalankan Proyek
+
+1. Clone repositori ini ke komputer Anda:
+
+   ```bash
+   git clone https://github.com/Bantas93/portofolio-html.git
+   Buka folder proyek:
+   ```
+
+bash
+Copy code
+cd repo-portofolio
+Buka file index.html di peramban web Anda.
+
+Fitur-Fitur
+Halaman utama menampilkan daftar proyek-proyek yang telah saya kerjakan.
+Setiap proyek memiliki deskripsi singkat dan tautan ke proyek tersebut.
+
+Teknologi :
+HTML
+CSS
+JavaScript
+API Gratis
+
+Proyek ini menggunakan API gratis untuk mendapatkan data tentang proyek-proyek yang ditampilkan di halaman utama. API yang digunakan adalah:
+
+base_url = https://api-ninjas.com/api
+hit_api = https://api.api-ninjas.com/v1/quotes?category=success
---
+++ main.js
@@ -0,0 +1,44 @@
+const category = "success";
+const apiUrl = "https://api.api-ninjas.com/v1/quotes?category=" + category;
+// Fungsi untuk mengambil data dari API
+function fetchData() {
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      displayDataQuote(data); // Memanggil fungsi untuk menampilkan data
+      displayDataAuthor(data); // Memanggil fungsi untuk menampilkan data
+    })
+    .catch((error) => {
+      console.error("Terjadi kesalahan:", error);
+    });
+}
+
+// Fungsi untuk menampilkan hasil dari API ke dalam elemen HTML dengan ID "apiData"
+function displayDataQuote(data) {
+  const apiDataElement = document.getElementById("quote");
+  apiDataElement.innerHTML = ""; // Mengosongkan elemen sebelum menambahkan data baru
+
+  data.forEach((quote) => {
+    const listQuote = document.createElement("p");
+
+    listQuote.textContent = `${quote.quote}`;
+    apiDataElement.appendChild(listQuote);
+  });
+}
+
+function displayDataAuthor(data) {
+  const apiDataElement = document.getElementById("author");
+  apiDataElement.innerHTML = "";
+  data.forEach((quote) => {
+    const listAuthor = document.createElement("p");
+    listAuthor.textContent = `${quote.author}`;
+    apiDataElement.appendChild(listAuthor);
+  });
+}
+// Panggil fetchData saat halaman dimuat
+setInterval(fetchData, 5000);
